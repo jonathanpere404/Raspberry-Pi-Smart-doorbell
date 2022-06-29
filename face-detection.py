1f950f5dbcda3c811cf6d70df9a4b46b5c8e6d22
jonathanpere404 <43252650+jonathanpere404@users.noreply.github.com>
2022-06-30 00:29:02 +0300

    Update face-detection.py

--- face-detection.py
+++ face-detection.py
@@ -13,36 +13,36 @@ from gpionappi import gp_sound_btn
 from screen_blank import blank_screen
 
 async def main():
-    bot = telebot.TeleBot("19*****:************QM") #<-------BUT YOUR TELEGRAM TOKEN/API KEY HERE
-    run('xset s 15', shell=True) #Screen will got to sleep mode after 15s
-    mouse = Controller() #Mouse position currently
-    old_mouse_position = mouse.position #To set up old mouse positio information
-    IMAGE_WIDTH = 1280 #Camera footage width 
-    IMAGE_HEIGHT = 720 #Camera footage lentght
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')#Mathematical metod to detect face
-    cap = cv2.VideoCapture(0)#To capture video from cam
-    cap.set(3, IMAGE_WIDTH)#Manually set the screen width
-    cap.set(4, IMAGE_HEIGHT)#Manually set the screen lentght
-    name = 'Photos' #File where to save images
-    img_counter = 0 #Used for image numbering
-    pic_save_cooldown = 0 #Used for picture cooldown to save image, one pic at the time 
-    GPIO.setmode(GPIO.BCM)#gpio button numberin mode setup
-    GPIO.setup(16, GPIO.IN,pull_up_down=GPIO.PUD_UP)#gpio button condition for press
-    screen_blank_ones = 0 #Initialise screen blanker to work once at a time
-    introduce_variable_once = 0 #Needs this to introduce variable to get the code running
-    secound_pic = 0 #Needs this to introduce variable to filter out blurry images
+    bot = telebot.TeleBot("19*****:************QM")       #<-------BUT YOUR TELEGRAM TOKEN/API KEY HERE
+    run('xset s 15', shell=True)                          #Screen will got to sleep mode after 15s
+    mouse = Controller()                                  #Mouse position currently
+    old_mouse_position = mouse.position                   #To set up old mouse positio information
+    IMAGE_WIDTH = 1280                                    #Camera footage width 
+    IMAGE_HEIGHT = 720                                    #Camera footage lentght
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')      #Mathematical metod to detect face
+    cap = cv2.VideoCapture(0)                             #To capture video from cam
+    cap.set(3, IMAGE_WIDTH)                               #Manually set the screen width
+    cap.set(4, IMAGE_HEIGHT)                              #Manually set the screen lentght
+    name = 'Photos'                                       #Folder where to save images
+    img_counter = 0                                       #Used for image numbering
+    pic_save_cooldown = 0                                 #Used for picture cooldown to save image, one pic at the time 
+    GPIO.setmode(GPIO.BCM)                                #gpio button numberin mode setup
+    GPIO.setup(16, GPIO.IN,pull_up_down=GPIO.PUD_UP)      #gpio button condition for press
+    screen_blank_ones = 0                                 #Initialise screen blanker to work once at a time
+    introduce_variable_once = 0                           #Needs this to introduce variable to get the code running
+    secound_pic = 0                                       #Needs this to introduce variable to filter out blurry images
     while True:
-        inputValue = GPIO.input(16)#Button bell function goes to this pin
-        if (inputValue == False):#Check button condition
+        inputValue = GPIO.input(16)                       #Button bell function goes to this pin
+        if (inputValue == False):                         #Check button condition
             doorbell = asyncio.create_task(gp_sound_btn())#Bell function
             await doorbell
-        _, img = cap.read()#Read image
+        _, img = cap.read()                               #Read image
         if (introduce_variable_once == 0): #This needs to be done to introduce screen_blank_timer
             screen_blank_timer = datetime.datetime.now() + datetime.timedelta(seconds=20)
             introduce_variable_once = 1
-        pos = mouse_pos(old_mouse_position)#To get mose position value
-        if(pos == True): #Check if mouse position is moved
-            old_mouse_position = mouse.position #To save mouse old position
+        pos = mouse_pos(old_mouse_position)               #To get mose position value
+        if(pos == True):                                  #Check if mouse position is moved
+            old_mouse_position = mouse.position           #To save mouse old position
             screen_blank_timer = datetime.datetime.now() + datetime.timedelta(seconds=18) 
             introduce_variable_once = 1        
         if (datetime.datetime.now() >= screen_blank_timer):
@@ -62,21 +62,21 @@ async def main():
                     secound_pic = 1
                 if (datetime.datetime.now() >= blurry_image_cooldown):
                     if (pic_save_cooldown == 0):
-                        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter) #Location for images
-                        cv2.imwrite(img_name, img) #Write image
-                        img_counter += 1 #Number pictures names
+                        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)   #Location for images
+                        cv2.imwrite(img_name, img)       #Write image
+                        img_counter += 1                 #Number pictures names
                         secound_pic = 0
-                        pic_save_cooldown = 1 #To get the cooldown going
-                        chat_id = 1*********5 # <-------BUT YOUR TELEGRAM CAHT ID HERE
-                        bot.send_photo(chat_id, open(img_name, 'rb'))#Send image to telegram
-                        t_end = datetime.datetime.now() + datetime.timedelta(seconds=30) #Cooldown to save and send
+                        pic_save_cooldown = 1            #To get the cooldown going
+                        chat_id = 1*********5            # <-------BUT YOUR TELEGRAM CAHT ID HERE
+                        bot.send_photo(chat_id, open(img_name, 'rb'))                     #Send image to telegram
+                        t_end = datetime.datetime.now() + datetime.timedelta(seconds=30)  #Cooldown to save and send
                     if (datetime.datetime.now() >= t_end):
-                        pic_save_cooldown = 0 #After this,,, ready for next image to save and send       
+                        pic_save_cooldown = 0            #After this,,, ready for next image to save and send       
         #Display
         cv2.imshow('img', img)
         #Stop if escape key is pressed
         key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"): #Press "Q" to quit program
+        if key == ord("q"):                              #Press "Q" to quit program
             cap.release()
             cv2.destroyAllWindows()
             break
